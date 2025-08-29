e39b8d52c59990956b06218f560a06727a47967b
Luke <121341736@umail.ucc.ie>
2025-08-29 16:08:50 +0100

    Removed unassociated files

--- Q1.py
+++
@@ -1,8 +0,0 @@
-def main():
-    real_number1 = float(input("Enter first number: "))
-    real_number2 = float(input("Enter second number: "))
-
-    origin_dist = (((real_number1)**2) + (real_number2)**2)**0.5
-
-    print(f'Distance from point of origin: {origin_dist:.2f}')
-main()
